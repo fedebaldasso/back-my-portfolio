e4fb3389b31133f644f5cb89d715c6e2ca8f87c8
Federico Baldasso <74937639+fedebaldasso@users.noreply.github.com>
2022-08-10 12:13:50 -0300

    Corrección de CORS

--- src/main/java/com/profile/Fede_Baldasso/Security/Controller/AuthController.java
+++ src/main/java/com/profile/Fede_Baldasso/Security/Controller/AuthController.java
@@ -38,7 +38,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "https://front-my-portfolio.web.app")
 public class AuthController {
 
     @Autowired
